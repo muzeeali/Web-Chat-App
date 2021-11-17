1db884361d6ac65372e85e19ead2ce48b4ee5fd5
muzeeali <muhammadzeeshanali8@gmail.com>
2021-11-17 19:20:46 +0500

    removed bugs

--- app.js
+++ app.js
@@ -1,140 +1,130 @@
-const socket = io("https://08d3-2400-adc1-13e-2400-646e-46e0-c500-1e19.ngrok.io");
+var socket;
 
 var foo = true;
+// console.log("UNIQUE ID", uuidv4());
 
-
-function validateEmail(email) 
-    {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    }
-function Name(){
-  var name= document.getElementById("name")
-return name.value
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
 }
-function Email(){
-  var email= document.getElementById("email")
-return email.value
+function Name() {
+  var name = document.getElementById("name");
+  return name.value;
 }
-function cNumber(){
-  var number= document.getElementById("number")
-return number.value
+function Email() {
+  var email = document.getElementById("email");
+  return email.value;
+}
+function cNumber() {
+  var number = document.getElementById("number");
+  return number.value;
 }
 function chatBtn() {
-   var name= Name()
-   var email= Email()
-   var number=cNumber()
+  var name = Name();
+  var email = Email();
+  var number = cNumber();
   var chat = document.getElementById("chatBox");
-  var info=  document.getElementById("info")
+  var info = document.getElementById("info");
 
-    if(name!=="" && email!==""&&number!=="")
-    {
-      chat.style.display = "block";
-     document.getElementById("msg").style.display="block";
-     document.getElementById("chat").style.display="block";
+  if (name !== "" && email !== "" && number !== "") {
+    chat.style.display = "block";
+    document.getElementById("msg").style.display = "block";
+    document.getElementById("chat").style.display = "block";
+  } else {
+    chat.style.display = "block";
 
-    }
-else 
-{
-  chat.style.display = "block"
- 
     info.style.display = "block";
-  
-}
-   
+  }
 }
 
-function cross(){
-    document.getElementById("chatBox").style.display = "none"
-    document.getElementById("info").style.display = "none"
-    document.getElementById("msg").style.display = "none"
-    document.getElementById("chat").style.display = "none"
-
- 
+function cross() {
+  document.getElementById("chatBox").style.display = "none";
+  document.getElementById("info").style.display = "none";
+  document.getElementById("msg").style.display = "none";
+  document.getElementById("chat").style.display = "none";
 }
 
-function msgBtn() { 
-
-  var name= Name()
-  var email= Email()
-  var number=cNumber()
+function msgBtn() {
+  var name = Name();
+  var email = Email();
+  var number = cNumber();
   var msg = document.getElementById("msg");
   var chat = document.getElementById("chat");
 
-  if(name!=="" && email!==""&&number!=="")
-  {
-      if(validateEmail(email)==false){
-        alert("Enter correct email")}
-        else{
-    document.getElementById("info").style.display = "none"
+  if (name !== "" && email !== "" && number !== "") {
+    if (validateEmail(email) == false) {
+      alert("Enter correct email");
+    } else {
+      document.getElementById("info").style.display = "none";
       msg.style.display = "block";
-      chat.style.display = "block";}
-    }
-  
-    else{
-  alert("Enter values")  
-    }
-  }
-
-
-
-
-
-
- 
-  
-
-$(document).ready(function(){
-  
-  function listenSocket(){
-    socket.on(cNumber(),function(msg){
-      console.log('listen',msg)
-      appendMessage(BOT_NAME, BOT_IMG, "left", msg);
-    })
-  }
-
-    const msgerForm = get(".msger-inputarea");
-    const msgerInput =get(".msger-input");
-    const msgerChat = get(".msger-chat");
-    
-    const BOT_MSGS = [
-      "Hi, how are you?",
-      "Ohh... I can't understand what you trying to say. Sorry!",
-      "I like to play games... But I don't know how to play!",
-      "Sorry if my answers are not relevant. :))",
-      "I feel sleepy! :("
-    ];
-    
-
-    const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
-    const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
-    const BOT_NAME = "Agent";
-    // const PERSON_NAME = "Customer";
-    
-  
-    $('.msger-send-btn').click(function(event){
-      if(foo){
-        listenSocket()
-        foo = false;
+      chat.style.display = "block";
+      let customerId;
+      if(localStorage.getItem('customerId')){
+        customerId = localStorage.getItem('customerId')
+      }else {
+        customerId = uuidv4();
+        localStorage.setItem('customerId',customerId)
       }
+      socket = io(`http://localhost:5000`);
+    }
+  } else {
+    alert("Enter values");
+  }
+}
 
-        console.log('clicked')
-      event.preventDefault();
-    
-      const msgText = msgerInput.value;
-      if (!msgText) return;
-      socket.emit('webChat-Customer', JSON.stringify({userId: cNumber(),customerName:Name(),message: msgText}))
-      appendMessage(Name(), PERSON_IMG, "right", msgText);
-      msgerInput.value = "";
-      // customerResponse()
-      // botResponse();
-    })
+$(document).ready(function () {
+  function listenSocket() {
+    socket.on(localStorage.getItem('customerId'), function (msg) {
+      console.log("listen", msg);
+      appendMessage(BOT_NAME, BOT_IMG, "left", msg);
+    });
+  }
 
-  
+  const msgerForm = get(".msger-inputarea");
+  const msgerInput = get(".msger-input");
+  const msgerChat = get(".msger-chat");
 
-    
-    function appendMessage(name, img, side, text) {
-      const msgHTML = `
+  const BOT_MSGS = [
+    "Hi, how are you?",
+    "Ohh... I can't understand what you trying to say. Sorry!",
+    "I like to play games... But I don't know how to play!",
+    "Sorry if my answers are not relevant. :))",
+    "I feel sleepy! :(",
+  ];
+
+  const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
+  const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
+  const BOT_NAME = "Agent";
+  // const PERSON_NAME = "Customer";
+
+  $(".msger-send-btn").click(function (event) {
+    if (foo) {
+      listenSocket();
+      foo = false;
+    }
+
+    console.log("clicked");
+    event.preventDefault();
+
+    const msgText = msgerInput.value;
+    if (!msgText) return;
+    socket.emit(
+      "webChat-Customer",
+      JSON.stringify({
+        phone: cNumber(),
+        userId: localStorage.getItem('customerId'),
+        customerName: Name(),
+        message: msgText,
+      })
+    );
+    appendMessage(Name(), PERSON_IMG, "right", msgText);
+    msgerInput.value = "";
+    // customerResponse()
+    // botResponse();
+  });
+
+  function appendMessage(name, img, side, text) {
+    const msgHTML = `
         <div class="msg ${side}-msg">
           <div class="msg-img" style="background-image: url(${img})"></div>
     
@@ -148,33 +138,33 @@ $(document).ready(function(){
           </div>
         </div>
       `;
-    
-      msgerChat.insertAdjacentHTML("beforeend", msgHTML);
-      msgerChat.scrollTop += 500;
-    }
-    
-    function botResponse() {
-      const r = random(0, BOT_MSGS.length - 1);
-      const msgText = BOT_MSGS[r];
-      const delay = msgText.split(" ").length * 100;
-    
-      setTimeout(() => {
-        appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
-      }, delay);
-    }
-    
-    function get(selector, root = document) {
-      return root.querySelector(selector);
-    }
-    
-    function formatDate(date) {
-      const h = "0" + date.getHours();
-      const m = "0" + date.getMinutes();
-    
-      return `${h.slice(-2)}:${m.slice(-2)}`;
-    }
-    
-    function random(min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
-    }
-})
+
+    msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+    msgerChat.scrollTop += 500;
+  }
+
+  function botResponse() {
+    const r = random(0, BOT_MSGS.length - 1);
+    const msgText = BOT_MSGS[r];
+    const delay = msgText.split(" ").length * 100;
+
+    setTimeout(() => {
+      appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
+    }, delay);
+  }
+
+  function get(selector, root = document) {
+    return root.querySelector(selector);
+  }
+
+  function formatDate(date) {
+    const h = "0" + date.getHours();
+    const m = "0" + date.getMinutes();
+
+    return `${h.slice(-2)}:${m.slice(-2)}`;
+  }
+
+  function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+});
